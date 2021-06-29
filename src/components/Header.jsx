8c5666e9b2d37e4990d7764b51fd3f7d85a82ff5
Viren <virenpatil1@outlook.com>
2021-06-29 18:36:55 +0200

    Created three components

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function Header() {
+  return (
+    <div>
+      <header>
+        <h1>Keeper</h1>
+      </header>
+    </div>
+  );
+}
---
+++ src/components/Note.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Note() {
+  return (
+    <div className="note">
+      <h1>Title of the note</h1>
+      <p>Content of the Note</p>
+    </div>
+  );
+}
